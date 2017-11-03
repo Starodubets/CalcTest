ab1de55f07ab4f1928c97f06856f520e69c66e50
Starodubets <31483344+Starodubets@users.noreply.github.com>
2017-11-03 14:22:01 +0200

    7

--- WindowsFormsCalc2Tests/CalculatorTests.cs
+++ WindowsFormsCalc2Tests/CalculatorTests.cs
@@ -1,27 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using WindowsFormsCalc2.Form1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace WindowsFormsCalc2 //.Form1.Tests
+
+namespace WindowsFormsCalc2
 {
     [TestClass()]
     public class CalculatorTests
     {
-        [DataTestMethod]
-        [DataRow(1, 2, '+', 3)]
-        [DataRow(1, 2, '*', 2)]
-        [DataRow(1, 2, '-', -1)]
-        [DataRow(2, 2, '/', 1)]
-
         [TestMethod()]
-        public void funct_calcTest(int a, int b, char op, int exp)
+        public void funct_calcTest11()
         {
-            int res = Form1.Calculator.funct_calc(a, b, op);
-            Assert.AreEqual(exp, res); ;
+            int res = Form1.Calculator.funct_calc(1, 2, '+');
+            Assert.AreEqual(3, res); ;
         }
     }
 }
