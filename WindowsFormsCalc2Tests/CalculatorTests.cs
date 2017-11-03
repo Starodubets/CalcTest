723647eec8040d6544edca6b607ae7cc0d7023a8
Starodubets <31483344+Starodubets@users.noreply.github.com>
2017-11-03 14:24:45 +0200

    9

--- WindowsFormsCalc2Tests/CalculatorTests.cs
+++ WindowsFormsCalc2Tests/CalculatorTests.cs
@@ -12,5 +12,23 @@ namespace WindowsFormsCalc2
             int res = Form1.Calculator.funct_calc(1, 2, '+');
             Assert.AreEqual(3, res); ;
         }
+        [TestMethod()]
+        public void funct_calcTest12()
+        {
+            int res = Form1.Calculator.funct_calc(5, 2, '-');
+            Assert.AreEqual(3, res); ;
+        }
+        [TestMethod()]
+        public void funct_calcTest13()
+        {
+            int res = Form1.Calculator.funct_calc(3, 2, '*');
+            Assert.AreEqual(6, res); ;
+        }
+        [TestMethod()]
+        public void funct_calcTest14()
+        {
+            int res = Form1.Calculator.funct_calc(6, 3, '*');
+            Assert.AreEqual(2, res); ;
+        }
     }
 }
