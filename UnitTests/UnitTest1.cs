bc40c3ef0f443d7569a8d316b856ddcc9a389954
Starodubets <31483344+Starodubets@users.noreply.github.com>
2017-11-03 14:05:49 +0200

    2

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace WindowsFormsCalc2
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, '+', 3)]
+        [DataRow(1, 2, '*', 2)]
+        [DataRow(1, 2, '-', -1)]
+        [DataRow(2, 2, '/', 1)]
+
+        [TestMethod()]
+        public void funct_calcTest(int a, int b, char op, int exp)
+        {
+            int res = Form1.Calculator.funct_calc(a, b, op);
+            Assert.AreEqual(exp, res); ;
+        }
+    }
+}
